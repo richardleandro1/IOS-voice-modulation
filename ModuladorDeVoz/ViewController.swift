4a19d68a343e801393442af91d76792f72b4af1a
richard <richardp.leandro@gmail.com>
2020-08-19 12:04:49 -0300

    Initial Commit

---
+++ ModuladorDeVoz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModuladorDeVoz
+//
+//  Created by Richard Leandro on 19/08/20.
+//  Copyright © 2020 Richard Leandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
